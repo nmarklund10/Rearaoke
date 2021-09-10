9804d96bb353fe3ab5452f5feeecb25fcba46ef9
user <user>
2021-09-09 21:32:29 -0400

    Add canvas element

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { createTheme } from '@material-ui/core/styles';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ExamplePage from './components/ExamplePage';
@@ -6,6 +6,8 @@ import HeadingBar from './components/HeadingBar';
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { light, dark, getCachedTheme } from './Themes';
 import { ThemeProvider } from '@material-ui/core';
+import Background from './components/Background';
+import createBackground from './js/webgl';
 
 export default function App() {
   const [isLightTheme, setTheme] = useState(getCachedTheme());
@@ -16,10 +18,15 @@ export default function App() {
     localStorage.setItem('theme', !isLightTheme);
   }
 
+  useEffect(() => {
+    createBackground();
+  }, []);
+
   return (
     <ThemeProvider theme={appliedTheme}>
       <CssBaseline />
       <HeadingBar isLightTheme={isLightTheme} parentCallback={handleCallback}/>
+      <Background/>
       <Router>
         <Switch>
           <Route path="/:msg">
