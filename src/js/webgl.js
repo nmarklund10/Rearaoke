49bab66aedd41c71d9de4e36c6ee9ce129b47568
user <user>
2021-09-12 19:29:27 -0400

    Add music notes

--- src/js/webgl.js
+++ src/js/webgl.js
@@ -1,41 +1,56 @@
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import keys_obj from '../static/Keys_Obj.obj';
 import * as THREE from 'three';
 
-export default function createBackground() {
-  const [scene, camera, renderer] = setupThreeJs();
+export default async function createBackground() {
+  const canvas = document.querySelector('#bg');
+  const [scene, camera, renderer] = setupThreeJs(canvas);
 
-  const torus = createTorus();
-  scene.add(torus);
+  const keyModels = await createNotes();
+  // keyModels[0].position.set(canvas.width / 8, canvas.height / 4, 0);
+  // keyModels[1].position.set(canvas.width / 8, 3 * (canvas.height / 4), 0);
+  // keyModels[2].position.set(7 * (canvas.width / 8), canvas.height / 4, 0);
+  // keyModels[3].position.set(7 * (canvas.width / 8), 3 * (canvas.height / 4), 0);
+  for (const [i, note] of keyModels.entries()) {
+    note.rotation.x += 1.5
+    // note.position.set(canvas.width / 8, canvas.height / 4, 0)
+    scene.add(note);
+  }
 
   const [pointLight, ambientLight] = createLights();
   scene.add(pointLight, ambientLight);
 
-  animate(scene, camera, renderer, torus);
+  animate(scene, camera, renderer, keyModels);
 }
 
-function setupThreeJs() {
-    const scene = new THREE.Scene();
+function setupThreeJs(canvas) {
+  const scene = new THREE.Scene();
 
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-    const renderer = new THREE.WebGLRenderer({
-        canvas: document.querySelector('#bg'),
-    });
+  const renderer = new THREE.WebGLRenderer({canvas: canvas});
 
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.position.setZ(30);
-    camera.position.setX(-3);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.position.setZ(30);
+  camera.position.setX(-3);
 
-    renderer.render(scene, camera);
+  renderer.render(scene, camera);
 
-    return [scene, camera, renderer];
+  return [scene, camera, renderer];
 }
 
-function createTorus() {
-    const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-    const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+async function createNotes() {
+    // instantiate a loader
+  const loader = new OBJLoader();
 
-    return new THREE.Mesh(geometry, material);
+  const keyData = await loader.loadAsync(keys_obj);
+  for (let i = 0; i < keyData.children.length; i++) {
+    const material = new THREE.MeshPhongMaterial({ color: 0xff6347 });
+    keyData.children[i] = new THREE.Mesh(keyData.children[i].geometry, material);
+  }
+
+  return keyData.children;
 }
 
 function createLights() {
@@ -47,14 +62,14 @@ function createLights() {
   return [pointLight, ambientLight];
 }
 
-function animate(scene, camera, renderer, torus) {
+function animate(scene, camera, renderer, meshes) {
   requestAnimationFrame(() => {
-    animate(scene, camera, renderer, torus);
+    animate(scene, camera, renderer, meshes);
   });
 
-  torus.rotation.x += 0.01;
-  torus.rotation.y += 0.005;
-  torus.rotation.z += 0.01;
+  for (const mesh of meshes) {
+    mesh.rotateX(0.01);
+  }
 
   renderer.render(scene, camera);
 }
