f5cc9041bdd88e854a9ddb2f2b37a8da7c198bbd
user <user>
2021-09-26 18:16:33 -0400

    Add audio control buttons

---
+++ src/components/KaraokePlayer/AudioProgress.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { LinearProgress } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  audioProgress: {
+    backgroundColor: theme.palette.primary.main,
+    width: '40vw',
+    height: theme.spacing(1),
+    borderRadius: theme.spacing(3),
+  },
+  audioFileName: {
+    float: 'right'
+  },
+  progressText: {
+    paddingTop: theme.spacing(1)
+  }
+}));
+
+export default function AudioProgress() {
+  const classes = useStyles();
+
+  return (
+    <>
+      <LinearProgress className={classes.audioProgress} variant="determinate" value={100}/>
+      <div className={classes.progressText}>
+        <span>.. / ..</span>
+        <span className={classes.audioFileName}>No audio file selected</span>
+      </div>
+    </>
+  );
+}
