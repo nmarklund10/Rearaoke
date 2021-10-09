6b1a6b1198509b8e3fd59903bfd67993a5413784
user <user>
2021-10-09 11:23:03 -0400

    add time helper functions

---
+++ src/js/time.js
@@ -0,0 +1,13 @@
+// 230.870204 -> 03:50.87
+export function secondsToString(seconds) {
+    var date = new Date(seconds * 1000)
+    return date.toISOString().substr(14, 8)
+}
+
+//03:45.00 -> 225
+export function stringToSeconds(timeString) {
+    var secs = parseInt(timeString.slice(0, 2)) * 60;
+    secs += parseInt(timeString.slice(3, 5));
+    secs += parseInt(timeString.slice(6)) / 100
+    return secs;
+}
