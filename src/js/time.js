8eaa6acd20b9fdf9360ed2951526a8c2973ac3c7
user <user>
2022-01-04 11:35:09 -0500

    fail lint for screenshot

--- src/js/time.js
+++ src/js/time.js
@@ -1,7 +1,7 @@
 // 230.870204 -> 03:50
 export function secondsToString(seconds) {
   var date = new Date(seconds * 1000);
-  return date.toISOString().substr(14, 5);
+  return date.toISOString().substr(14, 5)
 }
 
 //03:45.00 -> 225
