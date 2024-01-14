03ab286ba0e88761a4d35ef52fcc186bcb98ef04
nmarklund10 <nmarklund10@outlook.com>
2024-01-14 10:05:40 -0600

    change to stylistic ts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,38 +1,19 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: [
-    'react-refresh',
-    '@stylistic/js'
-  ],
+  plugins: ['react-refresh', '@stylistic/ts'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    indent: [
-      'error',
-      2
-    ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
-    quotes: [
-      'error',
-      'single'
-    ],
-    semi: [
-      'error',
-      'always'
-    ],
-    '@stylistic/js/object-curly-spacing': ["error", "always"]
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'comma-dangle': ['error', 'never'],
+    'max-len': ["error", { "code": 100 }],
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    '@stylistic/ts/object-curly-spacing': ['error', 'always'],
+    '@stylistic/ts/key-spacing': ['error', { beforeColon: false }]
   },
 };
