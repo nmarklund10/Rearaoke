4dd6c70885e2b5d94107324bca9aab5e68f868c4
nmarklund10 <nmarklund10@outlook.com>
2024-01-14 10:18:15 -0600

    lint

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,7 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-  setupFiles: [	'<rootDir>/src/setupTests.ts']
-
+  setupFiles: ['<rootDir>/src/setupTests.ts']
 };
 
 export default config;
