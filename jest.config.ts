067dad19872680c89863beb60bc2109e3add7d3f
nmarklund10 <nmarklund10@outlook.com>
2024-01-14 10:07:51 -0600

    lint

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  setupFiles: [	'<rootDir>/src/setupTests.ts']
+
+};
+
+export default config;
--- src/__tests__/App.test.ts
+++ src/__tests__/App.test.ts
@@ -1,11 +1,9 @@
-import React from 'react';
 import { render } from '@testing-library/react';
 import { configureStore } from '@reduxjs/toolkit';
 import {LyricWindow} from '../components/KaraokePlayer/LyricWindow';
-import {songReducer} from '../components/KaraokePlayer/songSlice';
+import songReducer from '../store/songSlice'
 import { Provider } from 'react-redux';
-import { readFileSync } from 'fs';
-import testLrc from './shivers.json';
+import testLrc from './mocks/shivers.json';
 
 test('check lyric window displays lyrics properly', () => {
   const initialState = testLrc;
